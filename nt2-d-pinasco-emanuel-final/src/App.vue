673f89d308135e32b1ae6ae0fde6220b3215bc8e
NeverwhereNowhere <manpinas14@gmail.com>
2022-07-12 20:14:54 -0300

    Resolución del final

---
+++ nt2-d-pinasco-emanuel-final/src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+     <TextStyle />
+
+     <br>
+     <br>
+     
+    <table class="table table-bordered">
+      <tr>
+        <th>Número de pregunta
+        </th>
+        <th>Respuesta</th>
+      </tr>
+      <tr>
+        <td>
+          1
+        </td>
+        <td>
+          b
+        </td>
+      </tr>
+      <tr>
+        <td>
+          2
+        </td>
+        <td>
+          c
+        </td>
+      </tr>
+      <tr>
+        <td>
+          3
+        </td>
+        <td>
+          b
+        </td>
+      </tr>
+      <tr>
+        <td>
+          4
+        </td>
+        <td>
+          d
+        </td>
+      </tr>
+      <tr>
+        <td>
+          5
+        </td>
+        <td>
+          d
+        </td>
+      </tr>
+
+    </table>
+  </div>
+</template>
+
+<script>
+import TextStyle from './components/Text.vue'
+
+export default {
+  name: 'App',
+  components: {
+     TextStyle
+  },
+
+}
+</script>
+
+<style>
+#app {
+  padding: 20px;
+}
+</style>
---
+++ nt2-d-pinasco-emanuel-final/src/components/Text.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <section class="src-components-text">
+    <br>
+
+    Color: <input type="text" v-model.trim="color" style="size: 200px">
+    Fondo: <input type="text" v-model.trim="fondo">
+    Size: <input type="text" v-model.trim="size">
+    <br>
+    <br>
+    <hr>
+    <br>
+    Texto: <input type="text" v-model.trim="text" :style="getStyle">
+    <br>
+    <br>
+    Cantidad de palabras {{getCantPalabras}}
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-text',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+        text: "",
+        fondo: "",
+        color: "",
+        size: ""
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+      getStyle() {
+        return `background-color: ${this.fondo}; color: ${this.color}; font-size: ${this.size}`
+      },
+      getCantPalabras() {
+        if(this.text.length) {
+          return this.text.split(" ").length
+        }
+        else {
+          return 0
+        }
+      }
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-text {
+    
+  }
+</style>
